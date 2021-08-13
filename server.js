9cf5f219bc1d3047f8a2b5d7d63544040b6b589e
woongstar <qualtypo@gmail.com>
2021-08-13 17:00:20 +0900

    add token in context

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ import schema from "./schema";
 
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI4ODM3MzgwfQ.UbyQykaOwEmEDArP-JhlW8ktxAvD816ojn2aoXnm7PE",
+  },
 });
 
 const PORT = process.env.PORT;
 
 server
   .listen(PORT)
-  .then(() => console.log("🙏Server is running on http://localhost:4000/ "));
+  .then(() => console.log(`🙏Server is running on http://localhost:${PORT}/`));
